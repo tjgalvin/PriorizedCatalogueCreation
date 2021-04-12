6249b3e2af5e088eb549513ad95aa8eaaa844d8c
Paul Hancock <Paul.Hancock@curtin.edu.au>
2021-04-12 16:42:28 +0800

    add an updated join_catalogues scirpt (from Robbie)

---
+++ join_catalogues.py
@@ -0,0 +1,144 @@
+#! /usr/bin/env python3
+
+import astropy
+from astropy.table import Table, Column
+from astropy.io.votable import from_table, writeto
+import numpy as np
+import glob
+import argparse
+import sys
+
+__author__ = "Paul Hancock"
+__date__ = "2021-04-12"
+
+def get_epoch_catalogues(epochs_file):
+    """
+    Read a file which contains a list of the catalogues to be read
+    in the format of "filename,prefix,suffix"
+    prefix/suffix may be empty/blank
+
+    parameters
+    ----------
+    epochs_files : str
+        A file which has a list of catalogues, one per line.
+
+    returns
+    -------
+    files : list
+        A list of filenames
+    prefix : list
+        A list of prefixes
+    suffix : list
+        A list of suffixes
+    """
+    lines = list(map(str.strip, open(epochs_file).readlines()))
+    words = [l.split(',')[:3] for l in lines if not l.startswith('#')]
+    files, prefix, suffix = zip(*words)
+    return files, prefix, suffix
+
+
+def join_catalogues(reference, epochs, prefix=None, suffix=None):
+    """
+    Take a reference cataloge, strip all but the uuid/ra/dec columns and then
+    join the flux/err_flux data from each of the epoch catalogues
+    From each epoch we extract the peak_flux and err_peak_flux columns and rename
+    them by appending _N where N is the epoch number
+
+    parameters
+    ----------
+    reference : str
+        Filename for the reference catalogue
+
+    epochs : list
+        A list of the individual epoch file names
+
+    prefix : list
+        Column name prefix for each epoch
+
+    suffix : list
+        Column name suffix for each epoch
+
+    returns
+    -------
+    table : `astropy.table.Table`
+        A joined table
+    """
+    # Read the reference catalogue and retain only the uuid and ra/dec columns
+    # rename the ra/dec columns
+    print("Using reference catalogue {0}".format(reference))
+    ref = Table.read(reference)['uuid', 'ra','dec']
+    ref.rename_column('ra', 'ref_ra')
+    ref.rename_column('dec', 'ref_dec')
+    ref.sort(keys='uuid')
+
+    # create lists for when prefix/suffix are None
+    if prefix is None:
+        prefix = ['']*len(ref)
+    if suffix is None:
+        suffix = ['']*len(ref)
+
+    # make the empty columns
+    new_cols =[]
+    data = np.zeros(len(ref), dtype=np.float32)
+
+    for i,(p,s) in enumerate(zip(prefix,suffix)):
+        for colname in ['{0}peak_flux{1}','{0}err_peak_flux{1}', '{0}local_rms{1}', '{0}background{1}']:
+            new_cols.append(Column(data=data.copy(), name=colname.format(p,s)))
+    print("ref table is {0} rows".format(len(ref)))
+    ref.add_columns(new_cols)
+
+    # now put the data into the new big table
+    for i,(f,p,s) in enumerate(zip(epochs,prefix,suffix)):
+        print("Joining epoch {0} catalogue {1}".format(i,f))
+        new_cols = Table.read(f)['uuid', 'peak_flux', 'err_peak_flux', 'local_rms', 'background',
+                                 'image', 'epoch']
+        new_cols.sort(keys='uuid')
+        # compute the order/presence
+        ordering = np.argwhere(np.in1d(ref['uuid'], new_cols['uuid'], assume_unique=True))[:,0]
+        ref['{0}peak_flux{1}'.format(p,s)][ordering] = new_cols['peak_flux']
+        ref['{0}err_peak_flux{1}'.format(p,s)][ordering] = new_cols['err_peak_flux']
+        ref['{0}local_rms{1}'.format(p,s)][ordering] = new_cols['local_rms']
+        ref['{0}background{1}'.format(p,s)][ordering] = new_cols['background']
+        ref['{0}image{1}'.format(p,s)][ordering] = new_cols['image']
+        ref['{0}epoch{1}'.format(p,s)][ordering] = new_cols['epoch']
+
+    return ref
+
+
+def write_table(tab, filename):
+    """
+    Write a VOTable using a binary format.
+
+    parameters
+    ----------
+    tab : `astropy.table.Table`
+        The table to be written
+
+    filename : str
+        The output filename. Should end with .xml or .vot
+    """
+    vot = from_table(tab)
+    vot.set_all_tables_format('binary')
+    vot.to_xml(filename)
+    print("Wrote {0}".format(filename))
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group1 = parser.add_argument_group("Combine catalogues into a flux table")
+    group1.add_argument("--refcat", dest='ref', type=str, default=None,
+                        help="The reference catalogue")
+    group1.add_argument("--epochs", dest='epochs', type=str, default=None,
+                        help="A file containing 'filename,prefix,suffix' for each catalogue.")
+    group1.add_argument("--out", dest='outfile', type=str, default='flux_table.vot',
+                        help="The output table name. Default = flux_table.vot")
+    results = parser.parse_args()
+
+    if None in (results.ref, results.epochs, results.outfile):
+        parser.print_help()
+        sys.exit()
+
+    files, prefix, suffix = get_epoch_catalogues(results.epochs)
+    table = join_catalogues(results.ref, files, prefix, suffix)
+    write_table(table, results.outfile)
